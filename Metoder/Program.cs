963573c8d795927702f259fd176de0be693677e9
GeorgeSmileyface <104126452+GeorgeSmileyface@users.noreply.github.com>
2025-03-10 12:32:03 +0100

    added actual code

--- Metoder/Program.cs
+++ Metoder/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿//lad os udregne areal
+PrintStylish("tast længde");
+string lengthString = Console.ReadLine();
+double length = Convert.ToDouble(lengthString);
+
+
+PrintStylish("tast bredde");
+string widthString = Console.ReadLine();
+double width = Convert.ToDouble(widthString);
+
+//double area = length * width;
+
+//Console.WriteLine(area);
+
+double methodResult = CalculateArea(length, width);
+
+Console.WriteLine(methodResult);
+
+string printThis = "hello John Connor";
+
+PrintStylish(printThis);
+
+double CalculateArea(double length, double width)
+{
+    double area = length * width;
+    return area;
+}
+
+void PrintStylish(string toPrint)
+{
+    foreach (char letter in toPrint)
+    {
+        Console.Write(letter);
+        Thread.Sleep(60);
+    }
+}
